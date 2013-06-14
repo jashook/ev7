4395edaea991f16407a03d70ebd3d6089b4aa982
= <=>
2013-06-14 16:51:56 -0500

    added compiler

---
+++ compiler/cross/lib/gcc/i586-elf/4.6.3/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-4.6.3/configure --target=i586-elf --prefix=/home/jarret/opt/cross --disable-nls --enable-languages=c,c++ --without-headers";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "pentium" }, { "arch", "pentium" } };
