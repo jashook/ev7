89f650e4124e4a40cd7847db0d8ec389939a375e
Jarret <jshook@smu.edu>
2013-06-17 10:00:46 -0500

    updated double_linked_list

--- os/t2/src/double_linked_list.c
+++ os/t2/src/double_linked_list.c
@@ -115,112 +115,155 @@ void** double_linked_list_front(double_linked_list* _List)
 void* double_linked_list_pop_back(double_linked_list* _List)
 {
 
-   int _Position;
-   bidirectional_node* _List_ptr = _List->_node;
-   void** _Return;
+   void* _Return;
 
-   for (_Position = 0; _Position < _List->_size - 1; ++_Position) _List_ptr = _List_ptr->_next;
+   _Return = _List->m_tail->m_prev->m_data
 
-   _Return = _List_ptr->_data;
-
-   if (_List->_size == 1)
+   if (_List->m_size == 1)
    {
 
-      free(_List->_node);
-      _List->_size = 0;
-      _List->_node = 0;
+      free(_List->m_tail->m_prev);
+      
+      _List->m_size = 0;
+      
+      _List->m_tail->m_prev = _List->m_head;
+   
+      _List->m_head->m_next = _List->m_tail;
+   
    }
 
    else
    {
-      _List_ptr = _List_ptr->_prev;
-      free(_List_ptr->_next);
-      _List_ptr->_next = 0;
-      --_List->_size;
+
+      _List->m_tail->m_prev = _List->m_tail->m_prev->m_prev;
+
+      free(_List->m_tail->m_prev->m_next);
+      
+      _List->m_tail->m_prev->m_next = _List->m_tail;
+      
+      --_List->m_size;
+   
    }
 
    return _Return;
 
 }
 
-void push_back_list(linked_list* _List, void* _Data)
+void double_linked_list_push_back(double_linked_list* _List, void* _Data)
 {
-   
-   int _Pos;
-   bidirectional_node* _List_ptr = _List->_node;
-   
-   for (_Pos = 0; _Pos < _List->_size - 1; ++_Pos) _List_ptr = _List_ptr->_next;
+
+   double_linked_list* _Prev;
 
    if (_List->_size == 0)
    {
-      _List->_node = (bidirectional_node*)malloc(sizeof(linked_list));
-      _List->_node->_next = 0;
-      _List->_node->_data = _Data;
-      ++_List->_size;
+
+      _List->m_head->m_next = (double_linked_list_node*)malloc(sizeof(double_linked_list_node));
+   
+      _List->m_head->m_next->m_next = _List->m_tail;
+
+      _List->m_tail->m_prev = _List->m_head->m_next;
+      
+      _List->m_head->m_next->m_data = _Data;
+      
+      ++_List->m_size;
+   
    }
 
    else
    {
-      _List_ptr->_next = (bidirectional_node*)malloc(sizeof(linked_list));
-      _List_ptr->_next->_next = 0;
-      _List_ptr->_next->_prev = _List_ptr;
-      _List_ptr->_next->_data = _Data;
-      ++_List->_size;
+
+      _Prev = _List->m_tail->m_prev;
+
+      _List->m_tail->m_prev = (double_linked_list_node*)malloc(sizeof(double_linked_list_node));
+      
+      _List->m_tail->m_prev->m_next = _List->m_tail;
+
+      _List->m_tail->m_prev->m_prev = _Prev;
+      
+      _Prev->m_next = _List->m_tail->m_prev;
+      
+      _List->m_tail->m_prev->m_data = _Data;
+      
+      ++_List->m_size;
+   
    }
 
 }
 
-void remove_from_list(linked_list* _List, int _Position)
+void double_linked_list_remove(double_linked_list* _List, int _Position)
 {
  
-   int _Pos;
-   bidirectional_node* _Next_ptr;
-   bidirectional_node* _Prev_ptr;
-   bidirectional_node* _List_ptr = _List->_node;
+   int _Count;
+   double_linked_list_node* _Pointer;
 
-   if (_Position > _List->_size || _List->_size < 0) return;
+   _Pointer = _List->m_head->m_next;
 
-   for (_Pos = 0; _Pos < _Position; ++_Pos) _List_ptr = _List_ptr->_next;
+   #if DEBUG
+
+      if (_Position > _List->m_size || _List->m_size == 0) return;
+
+   #endif
 
    if (_List->_size == 1)
    {
-      free(_List->_node);
-      _List->_node = 0;
-      _List->_size = 0;
+
+      free(_List->m_head->m_next);
+      
+      _List->m_head->m_next = _List->m_tail;
+
+      _List->m_tail->m_prev = _List->m_head;
+      
+      _List->m_size = 0;
+   
+      return;
+
    }
 
    else if(_Position == 0)
    {
-      _List->_node = _List_ptr->_next;
-      free(_List_ptr->_next->_prev);
+
+      _List->m_head->m_next = _List->m_head->m_next->m_next;
+      
+      free(_List->m_head->m_next->m_prev);
+
+      _List->m_head->m_next->m_prev = _List->m_head;
+      
       --_List->_size;
+
+      return;
+   
    }
 
    else if (_Position == _List->_size - 1)
    {
-      _Prev_ptr = _List_ptr->_prev;
-      free(_Prev_ptr->_next);
-      _Prev_ptr->_next = 0;
-      --_List->_size;
-   }
- 
-   else
-   {
-      _Next_ptr = _List_ptr->_next;
-      _Prev_ptr = _List_ptr->_prev;
- 
-      _Prev_ptr->_next = _Next_ptr;
-      free(_Next_ptr->_prev);
-      _Next_ptr->_prev = _Prev_ptr;
+
+      _List->m_tail->m_prev = _List->m_tail->m_prev->m_prev;
+
+      free(_List->m_tail->m_prev->m_next);
+
+      _List->m_tail->m_prev-m_next = _List->m_tail;
       
       --_List->_size;
+
+      return;
+   
    }
+
+   for (_Count = 0; _Count < _Position - 1; ++_Count) _Pointer = _Pointer->m_next;
+ 
+   _Pointer->m_next = _Pointer->m_next->m_next;
+
+   free(_Next_ptr->m_next->m_prev);
+
+   _Pointer->m_next->m_prev = _Pointer;
+      
+   --_List->m_size;
 
 }
 
-int size_of_list(linked_list* _List)
+int double_linked_list_size(double_linked_list* _List)
 {
 
-   return _List->_size;
+   return _List->m_size;
 
 }
